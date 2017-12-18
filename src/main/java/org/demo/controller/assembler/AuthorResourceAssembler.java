c5f59168b7b4c6e4020265d97c95380a06dc52cd
BROCHAIN <arnaud.brochain@sogeti.com>
2017-12-18 15:32:56 +0100

    adding selma mapper

--- src/main/java/org/demo/controller/assembler/AuthorResourceAssembler.java
+++ src/main/java/org/demo/controller/assembler/AuthorResourceAssembler.java
@@ -3,20 +3,25 @@ package org.demo.controller.assembler;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
 import org.demo.controller.AuthorController;
+import org.demo.controller.mapper.AuthorMapper;
 import org.demo.controller.resource.AuthorResource;
 import org.demo.entity.Author;
 import org.springframework.hateoas.mvc.ResourceAssemblerSupport;
 
+import fr.xebia.extras.selma.Selma;
+
 public class AuthorResourceAssembler extends ResourceAssemblerSupport<Author, AuthorResource> {
+
+	private AuthorMapper authorMapper;
 
 	public AuthorResourceAssembler() {
 		super(AuthorController.class, AuthorResource.class);
+		authorMapper = Selma.builder(AuthorMapper.class).build();
 	}
 
 	@Override
 	public AuthorResource toResource(Author entity) {
-		// FIXME improve mapping
-		final AuthorResource resource = new AuthorResource(entity.getFirstName(), entity.getLastName());
+		final AuthorResource resource = authorMapper.map(entity);
 		resource.setId(entity.getId());
 		resource.add(linkTo(AuthorController.class).slash(entity.getId()).withSelfRel());
 		return resource;
